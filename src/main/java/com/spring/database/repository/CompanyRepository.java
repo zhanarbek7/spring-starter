8f8e7b1ae0d64f71ed8ac3863da465c07ae009a8
zhanarbekabdurasulov <zhanarbek.a7@gmail.com>
2023-01-03 01:13:38 +0600

    Lesson 16 Custom beanPostProcessor

--- src/main/java/com/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/spring/database/repository/CompanyRepository.java
@@ -1,30 +1,11 @@
 package com.spring.database.repository;
 
+import com.spring.bpp.InjectBean;
 import com.spring.database.pool.ConnectionPool;
 
 public class CompanyRepository {
-    private final ConnectionPool connectionPool;
-    private String name;
 
-    public CompanyRepository(ConnectionPool connectionPool, String name) {
-        this.connectionPool = connectionPool;
-        this.name = name;
-    }
+    @InjectBean
+    private ConnectionPool connectionPool;
 
-    public static CompanyRepository of(ConnectionPool connectionPool, String name){
-        System.out.println("This is factory method");
-         return new CompanyRepository(connectionPool, name);
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "CompanyRepository{" +
-                "connectionPool=" + connectionPool +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
