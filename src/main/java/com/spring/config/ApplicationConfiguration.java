5831b66b74fd6ae74a6b7348014e9c3ddbeabd04
zhanarbekabdurasulov <zhanarbek.a7@gmail.com>
2023-01-05 03:21:57 +0600

    Java-based Configuration

---
+++ src/main/java/com/spring/config/ApplicationConfiguration.java
@@ -0,0 +1,23 @@
+package com.spring.config;
+import com.spring.database.repository.CrudRepository;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+@ComponentScan(value = "com.spring",
+        useDefaultFilters = false,
+        includeFilters = {
+                @Filter(type = FilterType.ANNOTATION, value = Component.class),
+                @Filter(type = FilterType.ASSIGNABLE_TYPE, value = CrudRepository.class),
+                @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository")
+        })
+public class ApplicationConfiguration {
+
+
+
+}
--- src/main/java/com/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/spring/database/repository/CompanyRepository.java
@@ -10,12 +10,14 @@ import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 @Transaction
 @Auditing
 public class CompanyRepository implements CrudRepository<Integer, Company>{
